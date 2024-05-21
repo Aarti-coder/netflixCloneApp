28b374ae539b369d56974ec20efdb4c60ded35e9
aarti.agrawal <aarti.agrawal@telekom-digital.com>
2024-05-21 19:34:24 +0530

    react initial setup

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-function main() {
-    console.log('------Triggered!-------')
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const App = () => {
+    return (
+        <div>this is a react Appp</div>
+    )
 }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />)
