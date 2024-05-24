d3464f108e2e12d4197bfc4f570646d5df14b97e
Deepali Srivastava <deepali.srivastava@telekom-digital.com>
2024-05-24 20:28:01 +0530

    tailwind + react-router install

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-const App = () => {
-    return (
-        <div>this is a react Appp</div>
-    )
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import LandingPage from "./src/containers/main";
+
+const AppLayout = () => {
+    return <div className="app">{
+        <>
+        <LandingPage />
+        <Outlet />
+        </>
+        }</div>
 }
 
+const appRoutes = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        errorElement: <h1>Landing Page Not Rendered!!</h1>,
+        children: [
+            {
+                path: '/about',
+                element: <h1>About!!</h1>,
+                errorElement: <h1>Landing Page Not Rendered!!</h1>
+            }
+        ]
+            
+    }
+])
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />)
+root.render(<RouterProvider router={appRoutes}/>)
---
+++ src/containers/main/index.js
@@ -0,0 +1,7 @@
+const LandingPage = () => {
+    return(
+        <div className="font-serif font-bold mt-12 bg-blue-500">Hello, my name is LandingPage!!!</div>
+    )
+}
+
+export default LandingPage
